0042355071d84a8993fdbe605af8f24a97e1a131
Sebastian Sunga <sebastian.sunga@my.csun.edu>
2025-03-05 22:37:51 -0800

    hashing out what our tokens are going to be, rought draft so add some if you spot any I missed

--- src/Type/Type.hs
+++ src/Type/Type.hs
@@ -4,3 +4,57 @@ module Type.Type
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+-- Date : 03/05/2025
+-- Explaination: The below text is me ordering out our tokens for so we have a guideline to follow for construction of our tokenizer.
+-- This isn't conclusive, so if you spot one that I missed, please put it in .
+-- The formating goes as : TokenName :: what is in the grammer on the Hasgull document.
+--  Consideration of what our tokens are.
+--  Symbols
+--
+-- Starting with symbols because that's easier to define.
+--
+--  EQUALSEQUALS :: `==`
+--  EQUALS :: `=`
+--  NOT_EQUALS :: `!=`
+--  GREATER_THAN :: `>`
+--  LESS_THAN :: `<`
+--  ADD :: `+`
+--  SUBTRACT :: `-`
+--  MULTIPLY :: `*`
+--  DIVIDE :: `/`
+--  LPAREN :: `(`
+-- RPAREN :: `)`
+-- LBRACE :: `{`
+-- RBRACE :: `}`
+-- COMMA :: `,`
+-- COLON :: `:`
+-- ARROW :: `=>`
+-- SEMICOLON :: `;`
+
+
+--  Reserved Words
+-- in relation with our grammer, this is any word that is within `` ticks or does not represent a rule.
+-- Int :: `Int`
+-- Void :: `Void`
+-- Boolean :: `Boolean`
+--  if :: `if`
+--  else :: `else`
+--  while :: `while`
+--  return :: `return`
+--  printLn :: `println`
+--  true :: `true`
+--  false :: `false`
+-- self :: `self`
+-- method :: `method`
+-- break :: `break`
+-- impl :: `impl`
+
+
+-- Identifiers
+-- Identifiers are any word that is not a reserved word or a symbol.
+-- Can this github copilot fuck off
+-- Integer(int) :: i
+-- Identifier(String) :: var
+-- StructName(String) :: structname
