4f7a0c81187bed1e7abe15de8171a3eb26016aac
angcortes <angelcortes@tutanota.com>
2025-04-10 14:10:22 -0700

    reformated Expr.hs

--- src/Parser/Expr.hs
+++ src/Parser/Expr.hs
@@ -1,96 +1,99 @@
-module Parser.Expr(
-   Expr(..)
-)where
+module Parser.Expr where
 
 import Tokenizer.Token (Token(..));
 
-    
-    data Program = 
-         ProgramItem | Stmt
+data Program =
+    ProgramItem
+  | Stmt
 
+data ProgramItem =
+    StructDef
+  | TraitDef
+  | Impldef
+  | FuncDef
 
-    data ProgramItem =
-         StructDef | TraitDef | Impldef | FuncDef
+data Exp =
+    EqualsExp
 
+data EqualsExp =
+    LessThanExp EqualsToken LessThanExp
+  | LessThanExp NotEqualToken LessThanExp
 
-    data Exp = 
-         EqualsExp
+data LessThanExp =
+    AddExp LessThanToken AddToken
 
-    data EqualsExp =
-          LessThanExp EqualsToken LessThanExp | LessThanExp NotEqualToken LessThanExp 
+data AddExp =
+    MultExp AddToken MultExp
+  | MultExp SubtractToken MultExp
 
-    data LessThanExp =
-          AddExp LessThanToken AddToken
+data MultExp =
+    CallExp MultiplyToken CallExp
+  | CallExp DivideToken CallExp
 
-    data AddExp =
-          MultExp AddToken MultExp | MultExp SubtractToken MultExp
-    
-    data MultExp =
-           CallExp MultiplyToken CallExp | CallExp DivideToken CallExp
+data CallExp =
+    DotExp LParenToken CommaExp RParenToken
 
-    data CallExp = 
-            DotExp LParenToken CommaExp RParenToken 
-    
-    data DotExp =
-             PrimaryExp DotToken var
+data DotExp =
+    PrimaryExp DotToken var
 
-    data PrimaryExp =
-             IntegerToken Int  | IdentifierToken String |
-             TrueToken | FalseToken | LowerCaseSelfToken 
-             | LParenToken Exp RParenToken | NewToken StructName LBraceToken StructActualParams RBraceToken
+data PrimaryExp =
+    IntegerToken Int
+  | IdentifierToken String
+  | TrueToken
+  | FalseToken
+  | LowerCaseSelfToken
+  | LParenToken Exp RParenToken
+  | NewToken StructName LBraceToken StructActualParams RBraceToken
 
-    data StructActualParams = 
-         StructActualParam CommaToken StructActualParam
+data StructActualParams =
+    StructActualParam CommaToken StructActualParam
 
-    data StructActualParam =
-        IdentifierToken String ColonToken Exp
+data StructActualParam =
+    IdentifierToken String ColonToken Exp
 
+data Stmt =
+    LetToken Param EqualToken Exp SemiColonToken
+  | IdentifierToken String EqualToken Exp SemiColonToken
+  | IfToken LParenToken Exp RParenToken Stmt ElseToken Stmt
+  | WhileToken LParenToken Exp RParenToken Stmt
+  | BreakToken SemiColonToken
+  | PrintLnToken LParenToken Exp RParenToken
+  | LBraceToken Stmt RBraceToken
+  | ReturnToken Exp SemiColonToken
+  | Exp SemiColonToken
 
-    data Stmt =
-         LetToken Param EqualToken Exp SemiColonToken |
-         IdentifierToken String EqualToken Exp SemiColonToken |
-         IfToken LParenToken Exp RParenToken Stmt ElseToken Stmt |
-         WhileToken LParenToken Exp RParenToken Stmt |
-         BreakToken SemiColonToken |
-         PrintLnToken LParenToken Exp RParenToken |
-         LBraceToken Stmt RBraceToken |
-         ReturnToken Exp SemiColonToken |
-         Exp SemiColonToken
+data FuncDef =
+    FuncDef IdentifierToken String LParenToken CommmaParam RParenToken ColonToken Type -- Da hell?
 
-    data FuncDef =
-        FuncDef IdentifierToken String LParenToken CommmaParam RParenToken ColonToken Type -- Da hell?
+data ImplDef =
+    ImplToken TraitName ForToken Type LBraceToken ConcMethodDef RParenToken
 
-    data ImplDef =
-        ImplToken TraitName ForToken Type LBraceToken ConcMethodDef RParenToken
+data TraitDef =
+    TraitToken TraitName LBraceToken AbsMethodDef RBraceToken
 
-    data TraitDef =
-         TraitToken TraitName LBraceToken AbsMethodDef RBraceToken
-    
-    data ConcMethodDef  =
-        MethodToken IdentifierToken String LParenToken CommmaParam RParenToken ColonToken Type LBraceToken Stmt RBraceToken
+data ConcMethodDef =
+    MethodToken IdentifierToken String LParenToken CommmaParam RParenToken ColonToken Type LBraceToken Stmt RBraceToken
 
-    data AbsMethodDef =
-        MethodToken IdentifierToken String LParenToken CommaParam RParenToken ColonToken Type SemiColonToken
+data AbsMethodDef =
+    MethodToken IdentifierToken String LParenToken CommaParam RParenToken ColonToken Type SemiColonToken
 
-    data StructDef =
-        StructToken  StructName LBraceToken COmmaParam RBraceToken
+data StructDef =
+    StructToken StructName LBraceToken COmmaParam RBraceToken
 
-    data CommaParam = 
-        Param CommaToken Param
+data CommaParam =
+    Param CommaToken Param
 
-    data Param =
-        IdentifierToken String ColonToken Type
+data Param =
+    IdentifierToken String ColonToken Type
 
-    data Type =
-        IntToken | VoidToken | Boolean |
-        SelfToken |
-        StructNameToken String |
-        LParenToken Type RParenToken |
-        LParenToken CommaType RParenToken ArrowToken Type
-        
-    data CommaType =
-        Type CommaToken Type
-   
+data Type =
+    IntToken
+  | VoidToken
+  | Boolean
+  | SelfToken
+  | StructNameToken String
+  | LParenToken Type RParenToken
+  | LParenToken CommaType RParenToken ArrowToken Type
 
-
-   
+data CommaType =
+    Type CommaToken Type
