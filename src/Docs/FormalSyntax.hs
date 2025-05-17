9aab94ab58f543c2adc3c8ce47aa38dca050c106
Bilin Mathew <bilinpattasseril@gmail.com>
2025-05-17 01:45:44 -0700

    Refactor FormalSyntax documentation for clarity and consistency

--- src/Docs/FormalSyntax.hs
+++ src/Docs/FormalSyntax.hs
@@ -9,95 +9,155 @@ EBNF-style notation. Copy and paste the following definitions as a reference.
 
 === Types and Identifiers
 
-* **var**  
-  Represents a variable.
+* __var__ Represents a variable.
 
-* **structname**  
-  Represents the name of a struct.
+* __structname__ Represents the name of a struct.
 
-* **traitname**  
-  Represents the name of a trait (typeclass).
+* __traitname__  Represents the name of a trait (typeclass).
 
-* **i**  
-  Represents an integer.
+* __i__  Represents an integer.
 
-=== Productions
+=== Concrete Syntax
+@
+__comma_type__ ::= [type (`,` type)*]
+@
 
-   comma_type ::= [type (`,` type)*]
 
-type ::=
-  `Int` | `Void` | `Boolean` | Built-in types
-  `Self` | Refers to our own type in a trait
-  structname | Structs are a valid kind of type
-  `(` type `)` | Parenthesized type
-  `(` comma_type `)` `=>` type Higher-order function
+@
+__type__ ::=
+         `Int` | `Void` | `Boolean` | /Built-in types/
+         `Self` | /Refers to our own type in a trait/
+         structname | /Structs are a valid kind of type/
+         `(` type `)` | /Parenthesized type/
+         `(` comma_type `)` `=>` type /Higher-order function/
+@
 
-param ::= var `:` type
 
-comma_param ::= [param (`,` param)*]
+@
+__param__ ::= var `:` type
+@
 
-structdef ::= `struct` structname `{` comma_param `}`
 
-Definition of an abstract method
-abs_methoddef ::= `method` var (` comma_param `)` `:` type `;`
+@
+__comma_param__ ::= [param (`,` param)*]
+@
 
-Definition of a concrete method
-conc_methoddef ::=
-  `method` var (` comma_param `)` `:` type `{` stmt* `}`
 
-Definition of a trait (typeclass)
-traitdef ::= `trait` traitname `{` abs_methoddef* `}`
+@
+__structdef__ ::= `struct` structname `{` comma_param `}`
+@
 
-Definition of an implementation of a typeclass
-impldef ::= `impl` traitname `for` type `{` conc_methoddef* `}`
 
-Definition of a toplevel function
-funcdef ::= `func` var `(` comma_param `)` `:` type
-            `{` stmt* `}` 
+@
+/Definition of an abstract method/
+__abs_methoddef__ ::= `method` var (` comma_param `)` `:` type `;`
+@
 
-stmt ::= `let` param `=` exp `;` | Variable declaration
-         var `=` exp `;` | Assignment
-         `if` `(` exp `)` stmt [`else` stmt] | if
-         `while` `(` exp `)` stmt | while
-         `break` `;` | break
-         `println` `(` exp `)` | Printing something
-         `{` stmt* `}` | Block
-         `return` [exp] `;` | Return
-         exp `;` Expression statements
 
-struct_actual_param ::= var `:` exp
+@
+/Definition of a concrete method/
+__conc_methoddef__ ::= `method` var (` comma_param `)` `:` type `{` stmt* `}`
+@
 
-struct_actual_params ::=
-  [struct_actual_param (`,` struct_actual_param)*]
 
-primary_exp ::= i | var | Integers and variables
-                `true` | `false` | Booleans
-                `self` | Instance on which we call a method
-                `(` exp `)` | Parenthesized expression
+@
+/Definition of a trait (typeclass)/
+__traitdef__ ::= `trait` traitname `{` abs_methoddef* `}`
+@
 
-                Creates a new instance of a struct
-                `new` structname `{` struct_actual_params `}`
-                
-Accessing a struct field or method
-dot_exp ::= primary_exp (`.` var)*
 
-call_exp ::= dot_exp (`(` comma_exp `)`)*
+@
+/Definition of an implementation of a typeclass/
+__impldef__ ::= `impl` traitname `for` type `{` conc_methoddef* `}`
+@
 
-mult_exp ::= call_exp ((`*` | `/`) call_exp)*
 
-add_exp ::= mult_exp ((`+` | `-`) mult_exp)*
+@
+/Definition of a toplevel function/
+__funcdef__ ::= `func` var `(` comma_param `)` `:` type `{` stmt* `}` 
+@
 
-less_than_exp ::= add_exp [`<` add_exp]
 
-equals_exp ::= less_than_exp [(`==` | `!=`) less_than_exp]
+@
+__stmt__ ::= 
+         `let` param `=` exp `;` | /Variable declaration/
+         var `=` exp `;` | /Assignment/
+         `if` `(` exp `)` stmt [`else` stmt] | /if/
+         `while` `(` exp `)` stmt | /while/
+         `break` `;` | /break/
+         `println` `(` exp `)` | /Printing something/
+         `{` stmt* `}` | /Block/
+         `return` [exp] `;` | /Return/
+         exp `;` /Expression statements/
+@
 
-exp ::= equals_exp
 
-program_item ::= structdef | traitdef | impldef | funcdef
+@
+__struct_actual_param__ ::= var `:` exp
+@
 
-program ::= program_item* stmt* stmt* is the entry point
+
+@
+__struct_actual_params__ ::= [struct_actual_param (`,` struct_actual_param)*]
+@
+
+
+@
+__primary_exp__ ::= 
+                 i | var | /Integers and variables/
+                 `true` | `false` | /Booleans/
+                 `self` | /Instance on which we call a method/
+                 `(` exp `)` | /Parenthesized expression/
+
+                 /Creates a new instance of a struct/
+                 `new` structname `{` struct_actual_params `}`
+@
+
+
+@
+/Accessing a struct field or method/
+__dot_exp__ ::= primary_exp (`.` var)*
+@
+
+
+@
+__call_exp__ ::= dot_exp (`(` comma_exp `)`)*
+@
+
+
+@
+__mult_exp__ ::= call_exp ((`*` | `/`) call_exp)*
+@
+
+
+@
+__add_exp__ ::= mult_exp ((`+` | `-`) mult_exp)*
+@
+
+
+@
+__less_than_exp__ ::= add_exp [`<` add_exp]
+@
+
+
+@
+__equals_exp__ ::= less_than_exp [(`==` | `!=`) less_than_exp]
+@
+
+
+@
+__exp__ ::= equals_exp
+@
+
+
+@
+__program_item__ ::= structdef | traitdef | impldef | funcdef
+@
+
+
+@
+__program__ ::= program_item* stmt* stmt* is the entry point
+@
 
 -}
 module Docs.FormalSyntax where
-
--- This module is used solely for documentation purposes.
